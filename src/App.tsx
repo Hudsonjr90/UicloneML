3832f3347c5ea3537afe0fd514a1687123aa19ff
Hudson Kennedy <hudsonhugo@bol.com.br>
2021-12-08 00:12:04 -0300

    startdev

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import GlobalStyle from './styles/GlobalStyles';
+
+import Layout from './components/Layout';
+
+function App() {
+  return (
+    <>
+      <Layout />
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
